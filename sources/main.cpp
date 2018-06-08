d884b8d3573ee288d9f9a1c5fc9f31f3b8602c37
Nett <37066846+nett7@users.noreply.github.com>
2018-06-08 23:42:25 +0300

    add the reversal of exceptions

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,14 +7,13 @@ using namespace std;
 int main() {
 
     size_t operation;
-    char met;
+    char print_method;
     int value;
     string answer;
     string path;
     Tree tree;
     setlocale(LC_ALL, "Russian");
-    cout<<"Работа с деревом\n\n";
-
+    cout << "Работа с деревом\n\n";
     while (true) {
         cout << "Выберите одну из операций:\n"
              << "1. Вывести дерево на экран\n"
@@ -27,88 +26,87 @@ int main() {
              << "8. Завершить работу программы\n";
 
         cin >> operation;
-        switch (operation) {
-            case 1 :
-                tree.print();
-                break;
-            case 2:{
-                cout << "Выберете метод обхода\n";
-                cout << "a. Прямой обход\n"
-                     << "b. Поперечный обход\n"
-                     << "c. Обратный обход\n";
-                bool key = false;
-                while (!key) {
-                    cin >> met;
-                    switch (met) {
-                        case 'a':
-                            tree.print_how(Tree::order::direct);
-                            key = true;
-                            break;
-                        case 'b' :
-                            tree.print_how(BSTree::Tree::order::reverse);
-                            key = true;
-                            break;
-                        case 'c' :
-                            tree.print_how(BSTree::Tree::order::symmetric);
-                            key = true;
-                            break;
-                             default:
-                                 cout << "Вы ввели неправильный метод, попробуйте еще\n";
-                                 break;
+        try {
+            switch (operation) {
+                case 1 :
+                    tree.print();
+                    break;
+                case 2: {
+                    cout << "Выберете метод обхода\n";
+                    cout << "a. Прямой обход\n"
+                         << "b. Поперечный обход\n"
+                         << "c. Обратный обход\n";
+                    bool key = false;
+                    while (!key) {
+                        cin >> print_method;
+                        switch (print_method) {
+                            case 'a':
+                                tree.print_how(Tree::order::direct);
+                                key = true;
+                                break;
+                            case 'b' :
+                                tree.print_how(BSTree::Tree::order::reverse);
+                                key = true;
+                                break;
+                            case 'c' :
+                                tree.print_how(BSTree::Tree::order::symmetric);
+                                key = true;
+                                break;
+                            default:
+                                cout << "Вы ввели неправильный метод, попробуйте еще\n";
+                                break;
+                        }
                     }
                 }
+                    break;
+                case 3:
+                    cout << "Введите значение для нового узла:\n";
+                    cin >> value;
+                    tree.insert(value);
+                    break;
+                case 4:
+                    cout << "Введите значение узла для удаления:\n";
+                    cin >> value;
+                    tree.remove(value);
+                    break;
+                case 5:
+                    cout << "Введите путь к файлу:\n";
+                    cin >> path;
+                    tree.save(path);
+                    break;
+                case 6:
+                    cout << "Введите путь к файлу:\n";
+                    cin >> path;
+                    break;
+
+                case 7:
+                    cout << "Введите значение для поиска:\n";
+                    cin >> value;
+                    tree.exist(value);
+                    break;
+
+                case 8:
+                    cout << "Вы уверены, что хотите выйти из программы ? (Да|Нет)\n";
+                    while (true) {
+                        cin >> answer;
+                        if ((answer == "Да") || (answer == "Нет"))
+                            break;
+                        else {
+                            cout << "Неправильный ввод ответа, попробуйте еще\n";
+                        }
+                    }
+                    break;
+                default:
+                    cout << "Неправильное значение метода, попробуйте еще\n";
+                    break;
             }
+            if (answer == "Да")
                 break;
-            case 3: cout<<"Введите значение для нового узла:\n";
-            cin>>value;
-                try {
-
-                tree.insert(value);
-                }
-                catch (logic_error err){
-                   cout<< err.what()<<endl;
-                }
-                break;
-            case 4: cout<<"Введите значение узла для удаления:\n";
-                cin>>value;
-                tree.remove(value);
-                break;
-            case 5: cout<<"Введите путь к файлу:\n";
-            cin>>path;
-            tree.save(path);
-                break;
-            case 6: cout<<"Введите путь к файлу:\n";
-                cin>>path;
-                try {
-                tree.load(path);
-                }
-                catch (logic_error err){
-                    cout<<err.what()<<endl;
-                }
-                break;
-
-            case 7: cout<<"Введите значение для поиска:\n";
-            cin>>value;
-                tree.exist(value);
-                break;
-
-            case 8:
-                cout << "Вы уверены, что хотите выйти из программы ? (Да|Нет)\n";
-                while (true) {
-                    cin >> answer;
-                    if ((answer == "Да") || (answer == "Нет"))
-                        break;
-                    else {
-                        cout << "Неправильный ввод ответа, попробуйте еще\n";
-                    }
-                }
-                break;
-
-                }
-        if (answer == "Да")
-            break;
         }
-
+        catch (logic_error &err) {
+            cout << err.what() << endl;
+        }
+    }
 
     return 0;
 }
